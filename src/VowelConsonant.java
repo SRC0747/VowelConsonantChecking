3062d1cf8543768c633bbefd02b930ae2292cc5a
SRC0747 <srcsrk1998@gmail.com>
2021-08-08 21:54:52 +0530

    Checking Vowel or consonant character.

---
+++ src/VowelConsonant.java
@@ -0,0 +1,15 @@
+public class VowelConsonant {
+
+    public static void main(String[] args)
+    {
+        char ch='R';
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' &&ch <= 'Z')) {
+            if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U')
+                System.out.println(ch + " is Vowel.");
+            else
+                System.out.println(ch + " is Consonant.");
+        }
+        else
+            System.out.println("Neither vowel nor consonant.");
+    }
+}
